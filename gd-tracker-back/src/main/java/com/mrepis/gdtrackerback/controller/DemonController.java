1e99ac7d59765bfd2a9076d02c6436d114f6a366
Louis Bedetti <louis.bedetti@univ-cotedazur.fr>
2026-02-22 16:11:03 +0100

    minor changes

--- gd-tracker-back/src/main/java/com/mrepis/gdtrackerback/controller/DemonController.java
+++ gd-tracker-back/src/main/java/com/mrepis/gdtrackerback/controller/DemonController.java
@@ -2,15 +2,19 @@ package com.mrepis.gdtrackerback.controller;
 
 import com.mrepis.gdtrackerback.model.Demon;
 import com.mrepis.gdtrackerback.repository.DemonRepository;
+import com.mrepis.gdtrackerback.service.DemonService;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/demons")
 public class DemonController {
     private final DemonRepository demonRepository;
+    private final DemonService demonService;
 
-    public DemonController(DemonRepository demonRepository) {
+    public DemonController(DemonRepository demonRepository, DemonService demonService) {
         this.demonRepository = demonRepository;
+        this.demonService = demonService;
     }
 
     @GetMapping
@@ -20,7 +24,7 @@ public class DemonController {
 
     @PostMapping
     public Demon addDemon(@RequestBody Demon demon) {
-        return demonRepository.save(demon);
+        return demonService.saveDemon(demon);
     }
 
     @GetMapping("/{id}")
@@ -33,6 +37,11 @@ public class DemonController {
     public void deleteDemon(@PathVariable Long id) {
         demonRepository.deleteById(id);
     }
+
+//    @PostMapping
+//    public Iterable<Demon> importDemons(@RequestPart FilePart filePart) {
+//
+//    }
 
 
 }
